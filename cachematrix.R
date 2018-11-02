431f8aab688188dbb84a6d0be5f41cc6913f3670
AlexDtSc <42781449+AlexDtSc@users.noreply.github.com>
2018-11-02 12:28:54 +0200

    Edited file
    Edited file with functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+#The following functions are used to create a special object that stores a matrix and caches its inverse. 
+
+
 ## Write a short comment describing this function
+
+# The first function, makeCacheMatrix () creates a special “matrix”, which is really a list containing a function to:
+
+#1) set the value of the matrix
+#2) get the value of the matrix
+#3) set the value of the inverse
+#4) get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
+# cacheSolve() function computes the inverse of the special “matrix” returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), then cacheSolve () should retrieve the inverse from the cache.
+
 cacheSolve <- function(x, ...) {
+    i <- x$getinverse()
+    if (!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+### Example for confirming the rightness of functions
+
+#Below we call the function with a matrix, compute the inverse, retrieve the inverse from the cache list, 
+#change the call matrix to the inverse, compute the inverse on that and return the original function.
+
+W <- matrix(c(2,5,4,5,8,3,5,9,3),3,3)
+W
+
+
+W1 <- makeCacheMatrix(W)
+cacheSolve(W1) #inverse returned after computation
+
+cacheSolve(W1) #inverse returned from cache
